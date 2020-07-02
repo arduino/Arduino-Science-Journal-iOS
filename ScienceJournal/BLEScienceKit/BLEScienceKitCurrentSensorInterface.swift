88499bf3b3d1116531bc2f688ffad3d26391c14a
Emilio Pavia <emilio.pavia@gmail.com>
2020-07-02 19:06:07 +0200

    Set tab width of 2 in project

--- ScienceJournal/BLEScienceKit/BLEScienceKitCurrentSensorInterface.swift
+++ ScienceJournal/BLEScienceKit/BLEScienceKitCurrentSensorInterface.swift
@@ -21,95 +21,95 @@ import UIKit
 import CoreBluetooth
 
 class BLEScienceKitCurrentSensorInterface: BLESensorInterface {
-    let characteristic: CBUUID
+  let characteristic: CBUUID
 
-    var identifier: String
+  var identifier: String
 
-    var serviceId: CBUUID
+  var serviceId: CBUUID
 
-    var providerId: String
+  var providerId: String
 
-    // FIXME: Localize
-    var name: String { "Current" }
+  // FIXME: Localize
+  var name: String { "Current" }
 
-    var iconName: String { "mkrsci_sensor_current" }
+  var iconName: String { "mkrsci_sensor_current" }
 
-    var animatingIconName: String { "mkrsci_current" }
+  var animatingIconName: String { "mkrsci_current" }
 
-    var config: Data?
+  var config: Data?
 
-    var peripheral: CBPeripheral?
+  var peripheral: CBPeripheral?
 
-    var unitDescription: String? { "A" }
+  var unitDescription: String? { "A" }
 
-    // FIXME: Localize
-    var textDescription: String {
-      "The amount of flow of charged " +
-      "particles between two places" }
+  // FIXME: Localize
+  var textDescription: String {
+    "The amount of flow of charged " +
+    "particles between two places" }
 
-    var hasOptions: Bool { false }
+  var hasOptions: Bool { false }
 
-    // FIXME: Change
-    var learnMoreInformation: Sensor.LearnMore = Sensor.LearnMore(firstParagraph: "",
-                                                                  secondParagraph: "",
-                                                                  imageName: "")
+  // FIXME: Change
+  var learnMoreInformation: Sensor.LearnMore = Sensor.LearnMore(firstParagraph: "",
+                                                                secondParagraph: "",
+                                                                imageName: "")
 
-    private var serviceScanner: BLEServiceScanner
-    private var peripheralInterface: BLEPeripheralInterface?
+  private var serviceScanner: BLEServiceScanner
+  private var peripheralInterface: BLEPeripheralInterface?
 
-    private lazy var clock = Clock()
+  private lazy var clock = Clock()
 
-    required init(providerId: String,
-                  identifier: String,
-                  serviceId: CBUUID,
-                  characteristic: CBUUID) {
-      self.providerId = providerId
-      self.characteristic = characteristic
-      self.identifier = identifier
-      self.serviceId = serviceId
-      self.serviceScanner = BLEServiceScanner(services: [serviceId])
-    }
+  required init(providerId: String,
+                identifier: String,
+                serviceId: CBUUID,
+                characteristic: CBUUID) {
+    self.providerId = providerId
+    self.characteristic = characteristic
+    self.identifier = identifier
+    self.serviceId = serviceId
+    self.serviceScanner = BLEServiceScanner(services: [serviceId])
+  }
 
-    func presentOptions(from viewController: UIViewController, completion: @escaping () -> Void) {
+  func presentOptions(from viewController: UIViewController, completion: @escaping () -> Void) {
 
-    }
+  }
 
-    func connect(_ completion: @escaping (Bool) -> Void) {
-      serviceScanner.connectToPeripheral(withIdentifier: identifier) { (peripheral, error) in
-        // Stop scanning.
-        self.serviceScanner.stopScanning()
+  func connect(_ completion: @escaping (Bool) -> Void) {
+    serviceScanner.connectToPeripheral(withIdentifier: identifier) { (peripheral, error) in
+      // Stop scanning.
+      self.serviceScanner.stopScanning()
 
-        guard peripheral != nil else {
-          print("[BluetoothSensor] Error connecting to " +
-                "peripheral: \(String(describing: error?.peripheral.name)) " +
-                "address: \(String(describing: error?.peripheral.identifier))")
-          // TODO: Pass along connection error http://b/64684813
-          completion(false)
-          return
-        }
-
-        self.peripheral = peripheral
-
-        completion(true)
+      guard peripheral != nil else {
+        print("[BluetoothSensor] Error connecting to " +
+          "peripheral: \(String(describing: error?.peripheral.name)) " +
+          "address: \(String(describing: error?.peripheral.identifier))")
+        // TODO: Pass along connection error http://b/64684813
+        completion(false)
+        return
       }
-    }
 
-    func startObserving(_ listener: @escaping (DataPoint) -> Void) {
-      guard let peripheral = peripheral else { return }
+      self.peripheral = peripheral
 
-      let interface = BLEPeripheralInterface(peripheral: peripheral,
-                                             serviceUUID: serviceId,
-                                             characteristicUUIDs: [characteristic])
-      interface.updatesForCharacteristic(characteristic, block: { [clock] data in
-        let float = data.withUnsafeBytes { $0.load(as: Float.self) }
-        let dataPoint = DataPoint(x: clock.millisecondsSince1970,
-                                  y: Double(float))
-        listener(dataPoint)
-      })
-      self.peripheralInterface = interface
+      completion(true)
     }
+  }
 
-    func stopObserving() {
-      peripheralInterface?.stopUpdatesForCharacteristic(characteristic)
-    }
+  func startObserving(_ listener: @escaping (DataPoint) -> Void) {
+    guard let peripheral = peripheral else { return }
+
+    let interface = BLEPeripheralInterface(peripheral: peripheral,
+                                           serviceUUID: serviceId,
+                                           characteristicUUIDs: [characteristic])
+    interface.updatesForCharacteristic(characteristic, block: { [clock] data in
+      let float = data.withUnsafeBytes { $0.load(as: Float.self) }
+      let dataPoint = DataPoint(x: clock.millisecondsSince1970,
+                                y: Double(float))
+      listener(dataPoint)
+    })
+    self.peripheralInterface = interface
+  }
+
+  func stopObserving() {
+    peripheralInterface?.stopUpdatesForCharacteristic(characteristic)
+  }
 }
