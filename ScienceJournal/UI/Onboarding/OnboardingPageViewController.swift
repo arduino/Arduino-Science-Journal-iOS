40765a49ff942814f81f27696559c02e3833e5cb
Emilio Pavia <e.pavia@arduino.cc>
2020-11-16 10:58:14 +0100

    Add onboarding scroll indicator

--- ScienceJournal/UI/Onboarding/OnboardingPageViewController.swift
+++ ScienceJournal/UI/Onboarding/OnboardingPageViewController.swift
@@ -40,5 +40,10 @@ class OnboardingPageViewController: UIViewController {
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     pageView.titleLabel.text = self.title
+
+    // we need to calculate the scroll view content size
+    // before updating the indicator
+    scrollView.layoutIfNeeded()
+    pageView.updateScrollViewIndicator()
   }
 }
