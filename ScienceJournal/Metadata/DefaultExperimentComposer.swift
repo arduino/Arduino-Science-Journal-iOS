22f0078feee5b7576aa906b46769e09b1f6117fb
Manuel Zomer <47141795+manuelzomer@users.noreply.github.com>
2020-11-27 12:40:54 +0100

    Update Default Experiment
    Adaptations of the default experiment to fit the new UI + onboarding screens.

--- ScienceJournal/Metadata/DefaultExperimentComposer.swift
+++ ScienceJournal/Metadata/DefaultExperimentComposer.swift
@@ -34,17 +34,9 @@ class DefaultExperimentComposer {
 
     //TODO localize content
     addIntroImage(experiment: experiment)
-    addIntroNote(experiment: experiment)
-    addNewExperimentNote(experiment: experiment)
-    addToolbarNote(experiment: experiment)
-    //addActionAreaNote(experiment: experiment)
-    addSensorNote(experiment: experiment)
-    addSensorSettingsNote(experiment: experiment)
-    addNotesNote(experiment: experiment)
-    addSnapshotNote(experiment: experiment)
-    addPhotosNote(experiment: experiment)
-    addActivitiesNote(experiment: experiment)
-    
+    addActionAreaImage(experiment: experiment)
+    addWebsiteImage(experiment: experiment)
+    addSidebarImage(experiment: experiment)
   }
   
   // swiftlint:disable line_length
@@ -54,124 +46,47 @@ class DefaultExperimentComposer {
       metaDataManager.saveImage(defaultExperimentPicture, atPicturePath: coverPicturePath, experimentID: experiment.ID)
       let pictureNote = PictureNote()
       pictureNote.timestamp = timestamp + 100
-      pictureNote.caption = Caption(text: "Shape your future! Reason with data and think like a real scientist! Turn your phone into a pocket science laboratory with tools to measure light, motion, sound and more!")
-             
-      //pictureNote.caption = Caption(text: String.firstExperimentPictureNoteCaption)
+      pictureNote.caption = Caption(text: "Welcome to Science Journal!")
       pictureNote.filePath = coverPicturePath
       experiment.notes.append(pictureNote)
       metaDataManager.updateCoverImageForAddedImageIfNeeded(imagePath: coverPicturePath, experiment: experiment)
     }
   }
   
-  private func addIntroNote(experiment: Experiment) {
-    //let textNote = TextNote(text: String.firstExperimentTextNote)
-    let textNote = TextNote(text: "Arduino Science Journal is a digital science journal that you can use to document projects through notes, photos, and your phone's built-in sensors, as well as external boards.")
-    textNote.timestamp = timestamp + 200
-    experiment.notes.append(textNote)
-  }
-  
-  private func addNewExperimentNote(experiment: Experiment) {
-    if let picture = UIImage(named: "default_experiment_new_experiment") {
-      let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_new_experiment")
-      metaDataManager.saveImage(picture, atPicturePath: picturePath, experimentID: experiment.ID)
-      let pictureNote = PictureNote()
-      pictureNote.timestamp = timestamp + 300
-      pictureNote.caption = Caption(text: "To create a new experiment, click on the + in the bottom of the Arduino Science Journal app home screen. Then click on the pencil icon in the top to give your experiment a good name!")
-       
-      //pictureNote.caption = Caption(text: String.firstExperimentPictureNoteCaption)
-      pictureNote.filePath = picturePath
-      experiment.notes.append(pictureNote)
-    }
-  }
-  
-  private func addToolbarNote(experiment: Experiment) {
+  private func addActionAreaImage(experiment: Experiment) {
     if let picture = UIImage(named: "default_experiment_toolbar") {
       let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_toolbar")
       metaDataManager.saveImage(picture, atPicturePath: picturePath, experimentID: experiment.ID)
       let pictureNote = PictureNote()
-      pictureNote.timestamp = timestamp + 400
-      pictureNote.caption = Caption(text: "The observation toolbar in the app can help you record the data around you. Let’s take a closer look!")
+      pictureNote.timestamp = timestamp + 200
+      pictureNote.caption = Caption(text: "Start taking notes and record data using the observation toolbar.")
       pictureNote.filePath = picturePath
       experiment.notes.append(pictureNote)
     }
   }
   
-  private func addActionAreaNote(experiment: Experiment) {
-    if let picture = UIImage(named: "default_experiment_action_area") {
-      let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_action_area")
+  private func addWebsiteImage(experiment: Experiment) {
+    if let picture = UIImage(named: "default_experiment_website") {
+      let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_website")
+      metaDataManager.saveImage(picture, atPicturePath: picturePath, experimentID: experiment.ID)
+      let pictureNote = PictureNote()
+      pictureNote.timestamp = timestamp + 300
+      pictureNote.caption = Caption(text: "Find activities and inspiration at science-journal.arduino.cc and start exploring the world around you.")
+      pictureNote.filePath = picturePath
+      experiment.notes.append(pictureNote)
+    }
+  }
+  
+  private func addSidebarImage(experiment: Experiment) {
+    if let picture = UIImage(named: "default_experiment_getting_started") {
+      let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_getting_started")
       metaDataManager.saveImage(picture, atPicturePath: picturePath, experimentID: experiment.ID)
       let pictureNote = PictureNote()
       pictureNote.timestamp = timestamp + 400
-      pictureNote.caption = Caption(text: "The observation toolbar in the app can help you record the data around you. Let’s take a closer look!")
+      pictureNote.caption = Caption(text: "If you missed the Getting Started guide or you want to go back to it, you can find it in the sidebar menu.")
       pictureNote.filePath = picturePath
       experiment.notes.append(pictureNote)
     }
-  }
-  
-  private func addSensorNote(experiment: Experiment) {
-    if let picture = UIImage(named: "default_experiment_sensor_button") {
-      let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_sensor_button")
-      metaDataManager.saveImage(picture, atPicturePath: picturePath, experimentID: experiment.ID)
-      let pictureNote = PictureNote()
-      pictureNote.timestamp = timestamp + 500
-      pictureNote.caption = Caption(text: "To start exploring your world by measuring light, sound, movement, and more, tap the Sensor button.")
-      pictureNote.filePath = picturePath
-      experiment.notes.append(pictureNote)
-    }
-  }
-  
-  private func addSensorSettingsNote(experiment: Experiment) {
-    if let picture = UIImage(named: "default_experiment_sensor_settings") {
-      let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_sensor_settings")
-      metaDataManager.saveImage(picture, atPicturePath: picturePath, experimentID: experiment.ID)
-      let pictureNote = PictureNote()
-      pictureNote.timestamp = timestamp + 600
-      pictureNote.caption = Caption(text: "You can manage your sensors in the Sensor settings screen. Go back to the sensor tab in the toolbar and tap the Gear icon on the sensor card.")
-      pictureNote.filePath = picturePath
-      experiment.notes.append(pictureNote)
-    }
-  }
-  
-  private func addNotesNote(experiment: Experiment) {
-    if let picture = UIImage(named: "default_experiment_note_button") {
-      let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_note_button")
-      metaDataManager.saveImage(picture, atPicturePath: picturePath, experimentID: experiment.ID)
-      let pictureNote = PictureNote()
-      pictureNote.timestamp = timestamp + 700
-      pictureNote.caption = Caption(text: "You can add notes and hypotheses to your experiments by tapping the Note icon.")
-      pictureNote.filePath = picturePath
-      experiment.notes.append(pictureNote)
-    }
-  }
-  
-  private func addSnapshotNote(experiment: Experiment) {
-    if let picture = UIImage(named: "default_experiment_snapshot_button") {
-      let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_snapshot_button")
-      metaDataManager.saveImage(picture, atPicturePath: picturePath, experimentID: experiment.ID)
-      let pictureNote = PictureNote()
-      pictureNote.timestamp = timestamp + 800
-      pictureNote.caption = Caption(text: "To make note of a single sensor measurement, tap the Snapshot Sensor button. You can also record sensor data over time, select a sensor and press the Record button to start capturing data.")
-      pictureNote.filePath = picturePath
-      experiment.notes.append(pictureNote)
-    }
-  }
-  
-  private func addPhotosNote(experiment: Experiment) {
-    if let picture = UIImage(named: "default_experiment_camera") {
-      let picturePath = metaDataManager.relativePicturePath(for: "default_experiment_camera")
-      metaDataManager.saveImage(picture, atPicturePath: picturePath, experimentID: experiment.ID)
-      let pictureNote = PictureNote()
-      pictureNote.timestamp = timestamp + 1000
-      pictureNote.caption = Caption(text: "Add camera shots, notes and photos to keep track of your observations while recording.")
-      pictureNote.filePath = picturePath
-      experiment.notes.append(pictureNote)
-    }
-  }
-  
-  private func addActivitiesNote(experiment: Experiment) {
-    let textNote = TextNote(text: "Find activities to try at\nscience-journal.arduino.cc\n\nNow that you know the basics of using Arduino Science Journal, you can use it to conduct experiments and explore your world!")
-    textNote.timestamp = timestamp  + 1100
-    experiment.notes.append(textNote)
   }
   
   // swiftlint:enable line_length
