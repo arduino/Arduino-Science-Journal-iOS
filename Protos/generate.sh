4a4d336de036df0621ed19b8d060dabcb4bd0a3e
Mateusz Ramski <mateusz@ramski.pl>
2021-04-28 15:14:11 +0200

    Moving protos generation from cocoapods in to build phase

--- Protos/generate.sh
+++ Protos/generate.sh
@@ -15,35 +15,36 @@
 #
 
 # Generates classes from proto files. This script should not be run
-# directly, it runs automatically as part of `pod install` instead.
+# directly, it runs automatically as part of build phase instead.
 
 #!/bin/bash
-
-OUTPUT_DIR="../"
-PROCESSING_DIR="third_party/sciencejournal/ios/Protos"
+OUTPUT_DIR="./"
+PROCESSING_DIR="third_party/sciencejournal/ios"
 
 # Make sure the output and processing directories exist or create them.
 mkdir -p "$OUTPUT_DIR"
-mkdir -p "$PROCESSING_DIR"
+mkdir -p "$PROCESSING_DIR"/Protos
 
-# Move the protos to the processing directory to work around import requirements.
-for file in *; do
-  if test -f "$file" && [ "$file" != $(basename $0) ] && [ "$file" != "ScienceJournalPortableFilter.pbascii" ]; then
-    mv "$file" "$PROCESSING_DIR/$file"
+# Copy the protos to the processing directory to work around import requirements.
+for file in Protos/*; do
+  if test -f "$file" && [ "$file" != Protos/$(basename $0) ] && [ "$file" != Protos/"ScienceJournalPortableFilter.pbascii" ]; then
+    cp "$file" "$PROCESSING_DIR/$file"
   fi
 done
 
-# Process the moved files.
-for file in "$PROCESSING_DIR"/*; do
+# Process the copied files.
+for file in "$PROCESSING_DIR"/Protos/*; do
   protoc "$file" --objc_out="$OUTPUT_DIR"
 done
 
-# Move the protos back to their normal directory.
-for file in "$PROCESSING_DIR"/*; do
-  mv "$file" "$(pwd)/${file##*/}"
+# Remove the processed protos.
+for file in "$PROCESSING_DIR"/Protos/*; do
+   if [ "${file: -6}" == ".proto" ]; then
+       rm "$file"
+   fi
 done
 
-# Clean the imports on the generated files to work in the open source project's hierarchy.
-for generatedFile in "$(pwd)/../third_party/sciencejournal/ios/Protos"/*; do
+# # Clean the imports on the generated files to work in the open source project's hierarchy.
+for generatedFile in "third_party/sciencejournal/ios/Protos"/*; do
   sed -i "" "s/#import \"third_party\/sciencejournal\/ios\/Protos\//#import \"/g" $generatedFile
 done
